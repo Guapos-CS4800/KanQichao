a15cf40c49a8fa8dc4d598e944cad272495c867c
didikid3 <didikid3@gmail.com>
2023-04-16 14:01:34 -0700

    Added Kanji Button JSON Response from Backend

--- src/demoCanvas_Backend.py
+++ src/demoCanvas_Backend.py
@@ -3,56 +3,78 @@ from flask_cors import CORS
 import subprocess
 import base64
 
+
+import testJisho as informer
 import classifier.classifyimage as classify
 
 
 app = Flask(__name__, template_folder='Templates', static_url_path='/static')
 CORS(app)
 
+
+#Home Page
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+#Use Route to Debug CI CD
 @app.route('/dong')
 def dong():
     return 'Hello World'
 
 
+#Main Functionality Page
+#Contains the cavas drawing element
 @app.route('/draw', methods=['GET', 'POST'])
 def draw():
 
     if request.method == 'POST':
         print("Sent Back Data")
-    
     else:
         return render_template('demoCanvas.html')
 
+'''
+Submit relates to when the user draws on the canvas
+On mouse release all drawing data is sent to backend to be processed
+Immediately after a GET request is sent to determine the results
+'''
 @app.route('/submit', methods=['GET','POST'])
 def cheetos():
     if request.method =="GET":
         print('SUBMIT URL: IN GET METHOD')
 
+        #Send the predicted characters back to front end
+        #Read the results from the file
         f = open("classifier/predictedkanji.txt", "r", encoding='utf-8')
         line_list = f.readlines()
         res = ""
         for line in line_list:
             res += line
+
         return res
 
     if request.method =='POST':
+        #Retreive Image information
         img = request.get_json()
         img = img['testing']
         
+        #Write Encoded Image into a text file for processing later
         FILE_WRITE = open("user_drawings/draw.txt", 'w')
         FILE_WRITE.write(img)
         FILE_WRITE.close()
 
-        #TODO: Make decoder for IMG URL to IMG
+        #Sanity Check
+        #Decode Image to verify it is the same as the drawing
         with open('sample.jpg', 'wb') as f:
             f.write(base64.decodebytes(img.split(',')[1].encode()))
         
+        #Given the image, predict the top ten associated kanji
+        #Results are put into a file predictedkanji.txt
         classify.main()
         
+        #Sanity Check
+        #Read the top ten kanji
         f = open("classifier/predictedkanji.txt", "r", encoding='utf-8')
         line_list = f.readlines()
         for line in line_list:
@@ -60,12 +82,30 @@ def cheetos():
 
         return "OK"
 
+'''
+Route Display is all functionality relating to pushing a button
+If the buttons for any of the predicted Kanji are clicked these methods
+are called.
+Main purpose is to generate relevant information to a specific kanji.
+'''
 @app.route('/display', methods=['GET','POST'])
 def displayKanjiInformation():
     if request.method == "POST":
         sentKanji = request.get_json()
         print(sentKanji['kanji'])
+        res = informer.getInfo(sentKanji['kanji'])
+
+        fileWrite = open("info.txt", "w", encoding='utf-8')
+        fileWrite.write(str(res))
+        fileWrite.close()
+
         return 'dong'
+    if request.method == 'GET':
+        fileRead = open("info.txt", "r", encoding="utf-8")
+        res = fileRead.read()
+        fileRead.close()
+        print(res)
+        return res
 
 
 if __name__ == '__main__':
--- src/testJisho.py
+++ src/testJisho.py
@@ -1,70 +1,75 @@
 import requests
-chart = "込"
 
-url = "https://jisho.org/search/" + chart + "%20%23kanji"
+def getInfo(character):
 
-response = requests.get(url)
+    url = "https://jisho.org/search/" + character + "%20%23kanji"
 
-if response.status_code == 200:
+    response = requests.get(url)
 
-    data = response.text
-
-    filewrite = open("info.txt", "w", encoding="utf-8")
-    filewrite.write(data)
-    filewrite.close()
-
-    def getReadingList(unparsed):
-        res = []
-        tmp = unparsed.split("</a>")
-        for i in range(len(tmp)-1):
-            res.append(tmp[i].split(">")[1])
-
-        return str(res)
+    japaneseChar, strokeCount, grade = "", "", ""
+    jlptLevel, english, kunReading = "", "", ""
+    onReading = ""
 
 
+    if response.status_code == 200:
 
-    data = data.split("main_results")[1]
-    japaneseChar = data.split("large-12 columns")[1:2]
-    print(japaneseChar)
-
-    strokeCount = data.split("kanji-details__stroke_count")[1].split("/strong")[0].split("strong>")[1].split("<")[0]
-    print(strokeCount)
-
-    grade = data.split("taught in")[1].split("</strong")[0].split("strong>")[1]
-    print(grade)
-
-    jlptLevel = data.split("JLPT level")[1].split("</")[0].split(">")[1]
-    print(jlptLevel)
+        data = response.text
 
 
-    english = data.split("row kanji-details--section")[1].split("kanji-details__main-meanings\">")[1]
-    english = english.split("</div>")[0].strip()
-    print(english)
+        def getReadingList(unparsed):
+            res = []
+            tmp = unparsed.split("</a>")
+            for i in range(len(tmp)-1):
+                res.append(tmp[i].split(">")[1])
+
+            return str(res)
 
 
-    try:
-        kunReading = data.split("<div class=\"kanji-details__main-readings\">")[1]
-        kunReading = kunReading.split("<dl class=\"dictionary_entry kun_yomi\">")[1]
-        kunReading = kunReading.split("<dd class=\"kanji-details__main-readings-list\" lang=\"ja\">")[1]
-        kunReading = kunReading.split("</dd>")[0].strip()
-        kunReading = getReadingList(kunReading)
-        print(kunReading)
-    except:
-        print("nada")
+
+        data = data.split("main_results")[1]
+
+        japaneseChar = data.split("large-12 columns")[1:2]
+
+        strokeCount = data.split("kanji-details__stroke_count")[1].split("/strong")[0].split("strong>")[1].split("<")[0]
+
+        grade = data.split("taught in")[1].split("</strong")[0].split("strong>")[1]
+
+        jlptLevel = data.split("JLPT level")[1].split("</")[0].split(">")[1]
+
+        english = data.split("row kanji-details--section")[1].split("kanji-details__main-meanings\">")[1]
+        english = english.split("</div>")[0].strip()
+
+        try:
+            kunReading = data.split("<div class=\"kanji-details__main-readings\">")[1]
+            kunReading = kunReading.split("<dl class=\"dictionary_entry kun_yomi\">")[1]
+            kunReading = kunReading.split("<dd class=\"kanji-details__main-readings-list\" lang=\"ja\">")[1]
+            kunReading = kunReading.split("</dd>")[0].strip()
+            kunReading = getReadingList(kunReading)
+        except:
+            print("nada")
+
+        
+
+        try:
+            onReading = data.split("<div class=\"kanji-details__main-readings\">")[1]
+            onReading = onReading.split("<dl class=\"dictionary_entry on_yomi\">")[1]
+            onReading = onReading.split("<dd class=\"kanji-details__main-readings-list\" lang=\"ja\">")[1]
+            onReading = onReading.split("</dd>")[0].strip()
+            onReading = getReadingList(onReading)
+        except:
+            print("e")
 
     
+    results = {}
+    results['japaneseChar'] = japaneseChar
+    results['strokeCount'] = strokeCount
+    results['grade'] = grade
+    results['jlptLevel'] = jlptLevel
+    results['english'] = english
+    results['kunReading'] = kunReading
+    results['onReading'] = onReading
 
-    try:
-        onReading = data.split("<div class=\"kanji-details__main-readings\">")[1]
-        onReading = onReading.split("<dl class=\"dictionary_entry on_yomi\">")[1]
-        onReading = onReading.split("<dd class=\"kanji-details__main-readings-list\" lang=\"ja\">")[1]
-        onReading = onReading.split("</dd>")[0].strip()
-        onReading = getReadingList(onReading)
-        print(onReading)
-    except:
-        print("e")
-
-   
+    return results
 
 
 
